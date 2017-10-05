8706ad5bf8592294d8986414ed91ae72197bd09a
José Soler Martínez <jsm134@alu.ua.es>
2017-10-05 11:45:34 +0200

    - Añadido Patron.java

---
+++ src/modelo/Patron.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author José Soler Martínez
+ *
+ */
+public class Patron {
+	private String nombre{}
+	public Patron(String, Tablero) {}
+	public String getNombre() {}
+	public EstadoCelda getCelda(Coordenada) {}
+	public Collection<Coordenada> getPosiciones(){}
+	@Override
+	public String toString() {
+		return "Patron []";
+	}
+}
