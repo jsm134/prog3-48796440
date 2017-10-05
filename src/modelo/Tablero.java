365ab4dcdd8dfc73a86bd29c34899aff9a85ea66
José Soler Martínez <jsm134@alu.ua.es>
2017-10-05 12:51:10 +0200

    Update

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -7,30 +7,40 @@ import java.util.HashMap;
 public class Tablero {
 	private HashMap<Coordenada,EstadoCelda> celdas;
 	public Tablero(Coordenada dimensiones){
-		
+		int x=dimensiones.getX();
+		int y=dimensiones.getY();
+		for (int i = 0; i < x+2; i++) {
+			for (int j = 0; j < y+2; j++) {
+				celdas.put(new Coordenada(i,j), EstadoCelda.MUERTA);
+			}
+		}
 	}
 	public Coordenada getDimensiones() {
-		
+		return null;
 	}
 	public Collection<Coordenada> getPosiciones(){
-		
+		return null;
 	}
 	public EstadoCelda getCelda(Coordenada posicion) {
+		return null;
 		
 	}
 	public void setCelda(Coordenada posicion, EstadoCelda e) {
 		
 	}
 	public ArrayList<Coordenada> getPosicionesVecinasCCW(Coordenada posicion){
+		return null;
 		
 	}
 	public void muestraErrorPosicionInvalida(Coordenada c) {
 		
 	}
 	public boolean cargaPatron(Patron patron, Coordenada coordenadaInicial) {
+		return false;
 		
 	}
 	public boolean contiene(Coordenada posicion) {
+		return false;
 		
 	}
 	@Override
