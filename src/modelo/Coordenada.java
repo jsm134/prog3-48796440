31a1bcbca070e57cd4c3e23bd6004e102bcdd172
Jose Soler Martinez <jsm134@alu.ua.es>
2017-10-28 14:21:40 +0200

    Eliminados Comentarios de Coordenada se restituiran al finalizar

--- src/modelo/Coordenada.java
+++ src/modelo/Coordenada.java
@@ -1,21 +1,11 @@
 package modelo;
 import modelo.excepciones.*;
-/**
- * Representa la clase Coordenada del proyecto, donde se realizan 
- * diferentes creaciones y modificaciones de las coordenadas.
- * @author José Soler Martínez
- * @version 1.0
- */
+
 public class Coordenada {
 	private int x;
 	private int y;
 	private static int NUMERO_COORDENADAS;
-	/**
-	 * Este es un constructor de coordenadas (x,y)
-	 * @param x Número entero que representa el parámetro x de la coordenada
-	 * @param y Número entero que representa la el parámetro y de la coordenada
-	 * Finalmente se suma 1 en NUMERO_COORDENADAS por la nueva coordenada creada
-	 */
+
 	public Coordenada(int x, int y) throws ExcepcionCoordenadaIncorrecta{
 		if(x<0 || y<0) {
 			throw new ExcepcionCoordenadaIncorrecta();
@@ -25,12 +15,7 @@ public class Coordenada {
 			NUMERO_COORDENADAS++;
 		}
 	}
-	/**
-	 * Este es un constructor de coordenadas (otra.x,otra.y)
-	 * @param otra creación una coordenada <code>otra</code> con parámetros (x,y) para poder tener 2 coordenadas
-	 * y así poder operarlas o trabajar con ellas (x,y) - (otra.x,otra.y)
-	 * Finalmente se suma 1 en NUMERO_COORDENADAS por una nueva coordenada creada
-	 */
+
 	public Coordenada (Coordenada otra) throws ExcepcionArgumentosIncorrectos {
 		if(otra == null) {
 			throw new ExcepcionArgumentosIncorrectos();
@@ -63,10 +48,7 @@ public class Coordenada {
 			return false;
 		return true;
 	}
-	/**
-	 * Este es un getter del número de coordenadas existente.
-	 * @return <code>NUMERO_COORDENADAS
-	 */
+
 	public static int getNumeroCoordenadas() {
 		return NUMERO_COORDENADAS;
 	}
@@ -74,27 +56,15 @@ public class Coordenada {
 	public String toString() {
 		return "(" + this.x + "," + this.y + ")";
 	}
-	/**
-	 * Getter de un parámetro de la coordenada
-	 * @return x
-	 */
+
 	public int getX() {
 		return x;
 	}
-	/**
-	 * Getter de un parámetro de la coordenada
-	 * @return y
-	 */
+
 	public int getY() {
 		return y;
 	}
-	/**
-	 * Permite la suma de los parámetros (x,y)+(otra.x,otra.y) y la creación de una coordenada nueva
-	 * @param otra segundo parámetro que se utiliza para operar
-	 * @return new Coordenada(x+otra.x,y+otra.y): un puntero con dos parámetros que llama al método
-	 * Coordenada(int x, int y)
-	 * @throws ExcepcionCoordenadaIncorrecta, ExcepcionArgumentosIncorrectos
-	 */
+
 	public Coordenada suma (Coordenada otra) throws ExcepcionCoordenadaIncorrecta, ExcepcionArgumentosIncorrectos {
 		if(otra==null) {
 			throw new ExcepcionArgumentosIncorrectos();
