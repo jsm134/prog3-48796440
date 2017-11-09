b7fb4a87dffc90692c265dcf484e7a3cd3822a95
Jose Soler Martinez <jsm134@alu.ua.es>
2017-11-09 13:42:29 +0100

    Comment Tablero2D

--- src/modelo/Tablero2D.java
+++ src/modelo/Tablero2D.java
@@ -5,6 +5,15 @@ import java.util.HashMap;
 import modelo.excepciones.*;
 
 public abstract class Tablero2D extends Tablero{
+	
+	/**
+	 * Inicializa un nuevo Tablero2D.
+	 *
+	 * @param ancho el ancho de la celda
+	 * @param alto el alto de la celda
+	 * @throws ExcepcionArgumentosIncorrectos la excepcion argumentos incorrectos
+	 * @throws ExcepcionCoordenadaIncorrecta la excepcion coordenada incorrecta
+	 */
 	public Tablero2D(int ancho, int alto) throws ExcepcionArgumentosIncorrectos, ExcepcionCoordenadaIncorrecta{
 		super(new Coordenada2D(ancho, alto));
 		celdas=new HashMap<Coordenada, EstadoCelda>();
